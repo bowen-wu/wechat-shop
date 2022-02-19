6b8bc803bebf3699f22156dc172f9a4ed2d5cb13
bowen-wu <13793248162@163.com>
2022-02-19 19:22:41 +0800

    fix: fixed OrderMapper & OrderGoodsMapper not found when start Order module
    1. move OrderMapper & OrderGoodsMapper from api module to Order module

--- wechat-shop-order/src/main/java/com/bowen/shop/order/dao/CustomOrderGoodsMapper.java
+++ wechat-shop-order/src/main/java/com/bowen/shop/order/dao/CustomOrderGoodsMapper.java
@@ -2,9 +2,11 @@ package com.bowen.shop.order.dao;
 
 import com.bowen.shop.api.generate.Order;
 import com.bowen.shop.api.generate.OrderGoods;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface CustomOrderGoodsMapper {
     void batchInsert(List<OrderGoods> orderGoodsList);
 
